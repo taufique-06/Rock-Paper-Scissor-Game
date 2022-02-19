8b828469ee6fa94159cb34a5a7872cdf2c5f9d11
taufique-06 <azad.taufique2020@gmail.com>
2022-02-19 16:24:57 +0000

    Update2.1

--- app/src/main/java/com/example/rockpaperscissor/GameActivity.java
+++ app/src/main/java/com/example/rockpaperscissor/GameActivity.java
@@ -3,9 +3,11 @@ package com.example.rockpaperscissor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,17 +16,30 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
+//    All variables starts here
 
+    ImageButton imageButtonOne,imageButtonTwo,imageButtonThree;
+    ImageView questionMarkImage, questionMarkImageTwo;
+    Button reset;
+    TextView playerText, computerText;
+    int playerWon = 0;
+    int computerWon = 0;
+    String computerWonText = "Computer Won! Better luck for next time!";
+    String playerWonText = "Congratulations! You Won";
 
+//    Ends here
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        ImageButton imageButtonOne = (ImageButton) findViewById(R.id.imageButton);
-        ImageButton imageButtonTwo = (ImageButton) findViewById(R.id.imageButton2);
-        ImageButton imageButtonThree = (ImageButton) findViewById(R.id.imageButton4);
-        ImageView questionMarkImageTwo = (ImageView) findViewById(R.id.imageView2);
 
+        imageButtonOne = (ImageButton) findViewById(R.id.imageButton);
+        imageButtonTwo = (ImageButton) findViewById(R.id.imageButton2);
+        imageButtonThree = (ImageButton) findViewById(R.id.imageButton4);
+        questionMarkImageTwo = (ImageView) findViewById(R.id.imageView2);
+        reset = (Button) findViewById(R.id.button6);
+
+//        When clicking first-button
         imageButtonOne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +49,7 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
+//        When clicking second-button
         imageButtonTwo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,6 +58,7 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
+//        When clicking third-button
         imageButtonThree.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,12 +66,20 @@ public class GameActivity extends AppCompatActivity {
                 computerMove();
             }
         });
+
+//        When clicking reset button
+
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resetScore();
+            }
+        });
     }
 
-
-    //    Creating ComputerMove() for getting computer's Input
+    //    Creating ComputerMove() for getting computer's Input starts here
     public void computerMove() {
-        ImageView questionMarkImage = (ImageView) findViewById(R.id.imageView);
+        questionMarkImage = (ImageView) findViewById(R.id.imageView);
         int random = new Random().nextInt(3);
         switch (random) {
             case 0:
@@ -72,33 +97,36 @@ public class GameActivity extends AppCompatActivity {
         }
     }
 
+//    Ends here
+
+//    findWinner() decides whether computer won or the player starts here
+
     public void findWinner(int number) {
-        ImageButton imageButtonOne = (ImageButton) findViewById(R.id.imageButton);
-        ImageButton imageButtonTwo = (ImageButton) findViewById(R.id.imageButton2);
-        ImageButton imageButtonThree = (ImageButton) findViewById(R.id.imageButton4);
-        TextView playerText = (TextView) findViewById(R.id.textView6);
-        TextView computerText = (TextView) findViewById(R.id.textView7);
-        int playerWon = 0;
-        int computerWon = 0;
+
+        imageButtonOne = (ImageButton) findViewById(R.id.imageButton);
+        imageButtonTwo = (ImageButton) findViewById(R.id.imageButton2);
+        imageButtonThree = (ImageButton) findViewById(R.id.imageButton4);
+        playerText = (TextView) findViewById(R.id.textView6);
+        computerText = (TextView) findViewById(R.id.textView7);
 
         if (imageButtonOne.isPressed()) {
-            computerWon = computerWon + 1;
-            playerWon = playerWon + 1;
+
             if (number == 0) {
 
-                Toast toast =  Toast.makeText(GameActivity.this, "Ooops! It is a draw", Toast.LENGTH_LONG);
-                toast.show();
+                showToast("Ooops! It is a draw");
             }
 
             if (number == 1) {
-                CharSequence text = "Computer Won! Better luck for next time!";
-                Toast.makeText(GameActivity.this, text, Toast.LENGTH_LONG).show();
-                computerText.setText("Player Two's Total Score: " + count(text));
+
+                showToast(computerWonText);
+//                Toast.makeText(GameActivity.this, text, Toast.LENGTH_LONG).show();
+                computerWon++;
+                computerText.setText("Player One's Total Score: " + computerWon);
             }
 
             if (number == 2) {
-                Toast.makeText(GameActivity.this, "Congratulations! You Won", Toast.LENGTH_LONG).show();
-
+                showToast(playerWonText);
+                playerWon++;
                 playerText.setText("Player One's Total Score: " + playerWon);
             }
 
@@ -106,50 +134,75 @@ public class GameActivity extends AppCompatActivity {
 
         } else if (imageButtonTwo.isPressed()) {
             if (number == 0) {
-                Toast.makeText(GameActivity.this, "Congratulations! You Won", Toast.LENGTH_LONG).show();
+                showToast(playerWonText);
                 playerWon++;
                 playerText.setText("Player One's Total Score: " + playerWon);
             }
 
             if (number == 1) {
-                Toast.makeText(GameActivity.this, "Ooops! It is a draw", Toast.LENGTH_LONG).show();
+                showToast("Ooops! It is a draw");
             }
 
             if (number == 2) {
-                Toast.makeText(GameActivity.this, "Computer Won! Better luck for next time!", Toast.LENGTH_LONG).show();
+                showToast(computerWonText);
                 computerWon++;
                 computerText.setText("Player Two's Total Score: " + computerWon);
             }
         } else if (imageButtonThree.isPressed()) {
             if (number == 0) {
-                Toast.makeText(GameActivity.this, "Computer Won! Better luck for next time!", Toast.LENGTH_LONG).show();
-                computerWon = computerWon +1;
+                showToast(computerWonText);
+                computerWon++;
                 computerText.setText("Player Two's Total Score: " + computerWon);
             }
 
             if (number == 1) {
-                Toast.makeText(GameActivity.this, "Congratulations! You Won", Toast.LENGTH_LONG).show();
-                playerWon = playerWon + 1;
+                showToast(playerWonText);
+                playerWon++;
                 playerText.setText("Player One's Total Score: " + playerWon);
             }
 
             if (number == 2) {
-                Toast.makeText(GameActivity.this, "Ooops! It is a draw", Toast.LENGTH_LONG).show();
+                showToast("Ooops! It is a draw");
             }
         }
 
     }
 
-    public int count(CharSequence text)
+//    Ends here
+
+//    resetScore() reset the score starts here
+
+    public void resetScore()
     {
-        int count = 0;
-        if (text == "Computer Won! Better luck for next time!")
-        {
-            count = count + 1;
-        }
-        else {
-            
-        }
-        return count;
+        questionMarkImageTwo = (ImageView) findViewById(R.id.imageView2);
+        questionMarkImage = (ImageView) findViewById(R.id.imageView);
+        playerText = (TextView) findViewById(R.id.textView6);
+        computerText = (TextView) findViewById(R.id.textView7);
+
+        playerText.setText("Player One's Total Score: 0");
+        computerText.setText("Player Two's Total Score: 0");
+        questionMarkImage.setImageDrawable(getResources().getDrawable(R.drawable.question));
+        questionMarkImageTwo.setImageDrawable(getResources().getDrawable(R.drawable.question));
     }
+
+//    Ends here
+
+    private void showToast(String text){
+        if (text == "Ooops! It is a draw")
+        {
+            Toast.makeText(this,text, Toast.LENGTH_SHORT).show();// this = Context
+        }
+        else
+        {
+            Toast.makeText(this,text, Toast.LENGTH_SHORT).show();// this = Context
+            MediaPlayer mediaplayer = MediaPlayer.create(GameActivity.this, R.raw.win);
+            mediaplayer.setOnCompletionListener(mediaPlayer -> {
+                mediaPlayer.reset();
+                mediaPlayer.release();
+            });
+            mediaplayer.start();
+        }
+
+    }
+
 }
